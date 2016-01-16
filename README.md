d7b39328d416f64955489c331076df1de9735869
agentzh2m <agentzh2m@gmail.com>
2016-01-16 12:35:08 +0700

    Change directory structure to fit IntelliJ working structure

--- README.md
+++
@@ -1,7 +0,0 @@
-# This is the speedy resumable thingy
-
-The file have not been compile yet this is just the source code you need
-to compile first before using these file but it is possible to
-use javac to compile TD to run the functions
-
-
---
+++ Source/TD.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Hamuel on 1/14/16.
+ */
+public class TD {
+    public static void main(String[] args) {
+        Main2.Second("http://docs.oracle.com/javase/7/docs/api/java/io/package-summary.html");
+        //TestSer.testSerIn("oracle.ser");
+        //TestSer.test1();
+    }
+}
